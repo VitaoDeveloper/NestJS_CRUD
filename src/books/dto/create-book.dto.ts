5206910d6b7adf0a94d1379b9a25423d9d989818
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-06-05 17:05:09 -0300

    Usando decorators ApiProperty para criação de schemas mais concisas

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength, MaxLength, IsOptional, IsUUID } from 'class-validator';
 import type { UUID } from 'crypto';
 
 export class CreateBookDto {
+  @ApiProperty({
+    description: 'Nome do livro',
+    example: 'Dom Casmurro',
+    maxLength: 80,
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(80)
   name: string;
 
+  @ApiProperty({
+    description: 'ID do gênero',
+    format: 'uuid'
+  })
   @IsOptional()
   @IsUUID()
   genreId?: UUID;
--- src/genres/dto/create-genre.dto.ts
+++ src/genres/dto/create-genre.dto.ts
@@ -1,8 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength, MaxLength } from 'class-validator';
 
 export class CreateGenreDto {
+  @ApiProperty({
+    description: 'Nome do gênero literário',
+    example: 'Romance',
+    maxLength: 80,
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(80)
-  name: string;
+  name: string
 }
